96dbc91a7b9cdd23caee609c6c04ee61ce18724b
Conrad-Yeung <70706378+Conrad-Yeung@users.noreply.github.com>
2020-12-15 12:07:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Bank Package - Aamir and Conrad", # Replace with your own username
-    version="0.0.1",
+    version="1.0.1",
     author="Aamir Khan & Conrad Yeung",
-    author_email="author@example.com",
+    author_email="conrad.yeung@hotmail.ca",
     description="This package can be used to manage bank customers (accounts and cards).",
     long_description="This package can be used to manage bank customers. There are two subpackages accounts and cards. Accounts subpackage is for corporate customers using bank for large transactions. Cards subpackage is for retail customers who would like to use their bank account with card transactions.",
     long_description_content_type="text/markdown",
